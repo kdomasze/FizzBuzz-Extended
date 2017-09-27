a49bb5d0b8724d63d8ea61ed9784b55d06ede2c2
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2017-09-27 15:39:51 -0700

    Adds basic implementation of FizzBuzz

--- FizzBuzz Extended/Program.cs
+++ FizzBuzz Extended/Program.cs
@@ -6,7 +6,20 @@ namespace FizzBuzz_Extended
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i < 100; i++)
+            {
+                string output = "";
+
+                if (i % 3 == 0) output += "Fizz";
+                if (i % 5 == 0) output += "Buzz";
+
+                if (output.Length == 0) output += i;
+                
+                Console.WriteLine(output);
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadLine();
         }
     }
 }
