a23f456dd4720a08834daf669eb23fc130af910e
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2017-09-27 16:27:36 -0700

    Adds parsing of a text file for determining range and replacement values of FizzBuzz

--- FizzBuzz Extended/Program.cs
+++ FizzBuzz Extended/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FizzBuzz_Extended
 {
@@ -6,20 +8,91 @@ namespace FizzBuzz_Extended
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 100; i++)
+            int start;
+            int end;
+            Dictionary<Int32, String> replacementsDictionary = new Dictionary<int, string>();
+
+            ParseConfiguration("Replacements.txt", out start, out end, replacementsDictionary);
+
+            for (int i = start; i <= end; i++)
             {
-                string output = "";
+                string output;
+                ReplaceValueFromDictionary(i, out output, replacementsDictionary);
 
-                if (i % 3 == 0) output += "Fizz";
-                if (i % 5 == 0) output += "Buzz";
-
-                if (output.Length == 0) output += i;
-                
                 Console.WriteLine(output);
             }
 
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadLine();
+        }
+
+        static void ReplaceValueFromDictionary(int input, out string output, Dictionary<Int32, String> replacementsDictionary)
+        {
+            output = "";
+
+            foreach (var replacement in replacementsDictionary)
+            {
+                if (input % replacement.Key == 0)
+                {
+                    output += replacement.Value;
+                }
+            }
+
+            if(output.Equals("")) output = input.ToString();
+        }
+
+        static void ParseConfiguration(string file, out int rangeStart, out int rangeEnd, Dictionary<Int32, String> replacemenDictionary)
+        {
+            rangeStart = 1;
+            rangeEnd = 100;
+
+            int currentLine = 0;
+            foreach (string line in File.ReadLines(file))
+            {
+                if (currentLine == 0)
+                {
+                    GetRange(line, out rangeStart, out rangeEnd);
+                }
+                else if(!String.IsNullOrEmpty(line))
+                {
+                    GetReplacement(line, replacemenDictionary);
+                }
+
+                currentLine++;
+            }
+        }
+
+        static void GetRange(string line, out int start, out int end)
+        {
+            string[] range = line.Split('-');
+
+            if (!Int32.TryParse(range[0], out start))
+            {
+                Console.WriteLine("Failed to parse start range: " + range[0] + ". Initializing with default value of " + start + ".\n");
+            }
+
+            if (!Int32.TryParse(range[1], out end))
+            {
+                Console.WriteLine("Failed to parse end range: " + range[1] + ". Initializing with default value of " + end + ".\n");
+            }
+        }
+
+        static void GetReplacement(string line, Dictionary<Int32, String> replacemenDictionary)
+        {
+            int replaceValue;
+            string replaceWord;
+
+            string[] replacement = line.Split(':');
+
+            if (!Int32.TryParse(replacement[0], out replaceValue))
+            {
+                Console.WriteLine("Failed to parse replace value: " + replacement[0] + ". Skipping entry.\n");
+                return;
+            }
+
+            replaceWord = replacement[1];
+
+            replacemenDictionary.Add(replaceValue, replaceWord);
         }
     }
 }
